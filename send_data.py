d056c47116dd4c27ec802e467a4acebf52cdc788
tharaka911 <tharakahitibandara@gmail.com>
2023-07-14 15:03:08 +0530

    files are working

---
+++ send_data.py
@@ -0,0 +1,18 @@
+import requests
+import json
+
+endpoint = "http://188.40.187.241:9000/aliens"
+
+for i in range(1, 1001):
+    data = {
+        "name": f"no{i}_lakshan",
+        "tech": f"tech{i}",
+        "sub": True
+    }
+
+    response = requests.post(endpoint, json=data)
+
+    if response.status_code == 200:
+        print(f"Data {i} sent successfully.")
+    else:
+        print(f"Failed to send data {i}. Error: {response.text}")
---
+++ send_data_concurrent.py
@@ -0,0 +1,34 @@
+import requests
+import json
+import threading
+
+endpoint = "http://188.40.187.241:9000/aliens"
+
+def send_data(i):
+    data = {
+        "name": f"no{i}_lakshan",
+        "tech": f"tech{i}",
+        "sub": True
+    }
+
+    response = requests.post(endpoint, json=data)
+
+    if response.status_code == 200:
+        print(f"Data {i} sent successfully.")
+    else:
+        print(f"Failed to send data {i}. Error: {response.text}")
+
+# Create a list to store the thread objects
+threads = []
+
+# Launch concurrent requests
+for i in range(1, 1001):
+    thread = threading.Thread(target=send_data, args=(i,))
+    thread.start()
+    threads.append(thread)
+
+# Wait for all threads to complete
+for thread in threads:
+    thread.join()
+
+print("All requests completed.")
